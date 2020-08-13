9512efc9c47becb7eed03bffec930edcd63bb42d
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-13 23:32:43 +0530

    [PM] Regenerated demo for pm_pm_wakeup_eic_sam_l21_xpro
    MH3-41304
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/pm/pm_wakeup_eic/firmware/src/config/sam_l21_xpro/peripheral/clock/plib_clock.c
+++ apps/pm/pm_wakeup_eic/firmware/src/config/sam_l21_xpro/peripheral/clock/plib_clock.c
@@ -49,9 +49,9 @@ static void OSCCTRL_Initialize(void)
 
 static void OSC32KCTRL_Initialize(void)
 {
-	OSC32KCTRL_REGS->OSC32KCTRL_OSC32K = 0x0;
+    OSC32KCTRL_REGS->OSC32KCTRL_OSC32K = 0x0;
 
-	OSC32KCTRL_REGS->OSC32KCTRL_RTCCTRL = OSC32KCTRL_RTCCTRL_RTCSEL(0);
+    OSC32KCTRL_REGS->OSC32KCTRL_RTCCTRL = OSC32KCTRL_RTCCTRL_RTCSEL(0);
 }
 
 static void DFLL_Initialize(void)
@@ -78,7 +78,7 @@ static void DFLL_Initialize(void)
 
     /* Configure DFLL    */
     OSCCTRL_REGS->OSCCTRL_DFLLCTRL = OSCCTRL_DFLLCTRL_ENABLE_Msk | OSCCTRL_DFLLCTRL_RUNSTDBY_Msk ;
-    
+
     while((OSCCTRL_REGS->OSCCTRL_STATUS & OSCCTRL_STATUS_DFLLRDY_Msk) != OSCCTRL_STATUS_DFLLRDY_Msk)
     {
         /* Waiting for DFLL to be ready */
@@ -89,7 +89,7 @@ static void DFLL_Initialize(void)
 
 static void GCLK0_Initialize(void)
 {
-    
+
     GCLK_REGS->GCLK_GENCTRL[0] = GCLK_GENCTRL_DIV(1) | GCLK_GENCTRL_SRC(6) | GCLK_GENCTRL_GENEN_Msk;
 
     while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL0_Msk) == GCLK_SYNCBUSY_GENCTRL0_Msk)
@@ -109,21 +109,21 @@ void CLOCK_Initialize (void)
     /*Initialize low Power Divider*/
     MCLK_REGS->MCLK_LPDIV = MCLK_LPDIV_LPDIV(0x01);
 
-    /*Initialize Backup Divider*/    
+    /*Initialize Backup Divider*/
     MCLK_REGS->MCLK_BUPDIV = MCLK_BUPDIV_BUPDIV(0x02);
 
     DFLL_Initialize();
     GCLK0_Initialize();
 
 
-	/* Selection of the Generator and write Lock for EIC */
+    /* Selection of the Generator and write Lock for EIC */
     GCLK_REGS->GCLK_PCHCTRL[3] = GCLK_PCHCTRL_GEN(0x0)  | GCLK_PCHCTRL_CHEN_Msk;
 
     while ((GCLK_REGS->GCLK_PCHCTRL[3] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
     {
         /* Wait for synchronization */
     }
-	/* Selection of the Generator and write Lock for SERCOM3_CORE */
+    /* Selection of the Generator and write Lock for SERCOM3_CORE */
     GCLK_REGS->GCLK_PCHCTRL[21] = GCLK_PCHCTRL_GEN(0x0)  | GCLK_PCHCTRL_CHEN_Msk;
 
     while ((GCLK_REGS->GCLK_PCHCTRL[21] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
