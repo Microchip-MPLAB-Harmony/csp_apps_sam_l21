e2dd73b7fadd229a21a1c1e0693a6c9a344eaea6
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-13 23:26:13 +0530

    [DMAC] Regenerated demo for dmac_dmac_usart_echo_sam_l21_xpro
    MH3-41292
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/dmac/dmac_usart_echo/firmware/src/packs/ATSAML21J18B_DFP/component/opamp.h
+++ apps/dmac/dmac_usart_echo/firmware/src/packs/ATSAML21J18B_DFP/component/opamp.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2020-03-04T04:55:13Z */
+/* file generated from device description version 2020-07-21T07:22:51Z */
 #ifndef _SAML21_OPAMP_COMPONENT_H_
 #define _SAML21_OPAMP_COMPONENT_H_
 
