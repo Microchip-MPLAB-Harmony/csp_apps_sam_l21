5e2370bd2e5afb6f2252753b4d8af1d8a4f00e94
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-01 16:26:53 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_sam_l21_xpro
    MH3-49529
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l21_xpro/initialization.c
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/config/sam_l21_xpro/initialization.c
@@ -118,6 +118,7 @@
 
 void SYS_Initialize ( void* data )
 {
+
     NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
     PM_Initialize();
@@ -134,6 +135,8 @@ void SYS_Initialize ( void* data )
     NVMCTRL_Initialize( );
 
     SERCOM2_I2C_Initialize();
+
+    EVSYS_Initialize();
 
 
 
