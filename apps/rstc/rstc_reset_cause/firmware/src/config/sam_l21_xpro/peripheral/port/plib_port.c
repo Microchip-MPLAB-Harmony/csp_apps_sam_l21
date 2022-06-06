078a027d0f12dd7ea3d2b2d34439aeff73ee259e
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-06-06 13:06:42 +0530

    [RSTC] Regenerated demo for rstc_rstc_reset_cause_sam_l21_xpro
    MH3-68209
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/rstc/rstc_reset_cause/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/rstc/rstc_reset_cause/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -77,6 +76,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[1] = 0x0;
    PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
 
    /************************** GROUP 1 Initialization *************************/
@@ -84,9 +84,10 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_OUT = 0x400;
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x0;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
