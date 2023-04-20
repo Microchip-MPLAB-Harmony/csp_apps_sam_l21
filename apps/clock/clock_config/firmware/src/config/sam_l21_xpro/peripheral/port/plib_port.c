208a347254a9b3d9c6b2b7afeaf3efdd3b88bf5e
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-20 10:37:47 +0530

    [CLOCK] Regenerated demo for clock_clock_config_sam_l21_xpro
    MH3-79675

--- apps/clock/clock_config/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/clock/clock_config/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -74,13 +74,13 @@ void PORT_Initialize(void)
 
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_DIR = 0x400;
-   PORT_REGS->GROUP[1].PORT_OUT = 0x400;
-   PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x0;
-   PORT_REGS->GROUP[1].PORT_PINCFG[15] = 0x1;
+   PORT_REGS->GROUP[1].PORT_DIR = 0x400U;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x400U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x0U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[15] = 0x1U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x0;
-   PORT_REGS->GROUP[1].PORT_PMUX[7] = 0x70;
+   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x0U;
+   PORT_REGS->GROUP[1].PORT_PMUX[7] = 0x70U;
 
 
 }
