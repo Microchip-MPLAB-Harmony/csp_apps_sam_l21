a3012f44008791b8bc9bca3b29d581b34072c760
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-20 10:38:05 +0530

    [PM] Regenerated demo for pm_pm_wakeup_eic_sam_l21_xpro
    MH3-79685

--- apps/pm/pm_wakeup_eic/firmware/src/config/sam_l21_xpro/peripheral/clock/plib_clock.c
+++ apps/pm/pm_wakeup_eic/firmware/src/config/sam_l21_xpro/peripheral/clock/plib_clock.c
@@ -41,6 +41,8 @@
 #include "plib_clock.h"
 #include "device.h"
 
+
+
 static void OSCCTRL_Initialize(void)
 {
     /**************** OSC16M IniTialization *************/
@@ -65,7 +67,7 @@ static void DFLL_Initialize(void)
     }
 
     /*Load Calibration Value*/
-    uint8_t calibCoarse = (uint8_t)(((*(uint32_t*)0x806020U) >> 26U ) & 0x3fU);
+    uint8_t calibCoarse = (uint8_t)(((*(uint32_t*)0x00806020U) >> 26U ) & 0x3fU);
 
     OSCCTRL_REGS->OSCCTRL_DFLLVAL = OSCCTRL_DFLLVAL_COARSE((uint32_t)calibCoarse) | OSCCTRL_DFLLVAL_FINE(512U);
 
@@ -81,6 +83,7 @@ static void DFLL_Initialize(void)
     {
         /* Waiting for DFLL to be ready */
     }
+    
 }
 
 
@@ -104,11 +107,11 @@ void CLOCK_Initialize (void)
     /* Function to Initialize the 32KHz Oscillators */
     OSC32KCTRL_Initialize();
 
-    /*Initialize low Power Divider*/
-    MCLK_REGS->MCLK_LPDIV = MCLK_LPDIV_LPDIV(0x01U);
-
     /*Initialize Backup Divider*/
     MCLK_REGS->MCLK_BUPDIV = MCLK_BUPDIV_BUPDIV(0x02U);
+
+    /*Initialize low Power Divider*/
+    MCLK_REGS->MCLK_LPDIV = MCLK_LPDIV_LPDIV(0x01U);
 
     DFLL_Initialize();
     GCLK0_Initialize();
