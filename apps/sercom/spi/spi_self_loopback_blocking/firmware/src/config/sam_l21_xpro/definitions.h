645c0d1fe5174eebf6ae65b48897dc680a5dad90
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-16 14:07:52 +0530

    [SERCOM] Regenerated demo for sercom__spi__spi_self_loopback_blocking__firmware__sam_l21_xpro.X
    MH3-102282

--- apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l21_xpro/definitions.h
@@ -53,8 +53,8 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/sercom/spi_master/plib_sercom5_spi_master.h"
 #include "peripheral/pm/plib_pm.h"
+#include "peripheral/sercom/spi_master/plib_sercom5_spi_master.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -65,13 +65,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAML21J18B"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAML"
-#define DEVICE_SERIES		 "SAML21"
+#define DEVICE_NAME          "ATSAML21J18B"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAML"
+#define DEVICE_SERIES        "SAML21"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
